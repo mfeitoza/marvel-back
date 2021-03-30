45350b86d63ffb3e8a8c5d42d6a2d2c6d8827780
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-03-30 16:23:40 -0300

    feat(services): add user service

---
+++ marvel/src/main/java/com/marvel/marvel/services/UserService.java
@@ -0,0 +1,47 @@
+package com.marvel.marvel.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import com.marvel.marvel.entities.User;
+import com.marvel.marvel.entities.Document;
+import com.marvel.marvel.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+  
+  @Autowired
+  private AuthService authService;
+
+  public Optional<User> create(String name, String email, String password, String phone, String gender,
+  Date birthday, List<Document> documents) {
+    User user = new User(name, email, authService.encodePassword(password), phone, gender, birthday, documents);
+    userRepository.save(user);
+    Optional<User> userData = userRepository.findById(user.getId());
+    return userData;
+  }
+
+  public Optional<User> create(String name, String email, String password, String phone, String gender,
+  Date birthday) {
+    User user = new User(name, email, authService.encodePassword(password), phone, gender, birthday);
+    userRepository.save(user);
+    Optional<User> userData = userRepository.findById(user.getId());
+    return userData;
+  }
+
+  public List<User> findAll() {
+    return userRepository.findAll();
+  }
+
+  public Optional<User> findById(String id) {
+    return userRepository.findById(id);
+  }
+  
+}
