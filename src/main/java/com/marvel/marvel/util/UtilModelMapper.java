6452c92691249343f489205132560a827cca8ef9
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-03-31 01:46:12 -0300

    feat(util): add model mapper

---
+++ src/main/java/com/marvel/marvel/util/UtilModelMapper.java
@@ -0,0 +1,19 @@
+package com.marvel.marvel.util;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.modelmapper.ModelMapper;
+
+public final class UtilModelMapper {
+
+  static ModelMapper modelMapper = new ModelMapper();
+
+  private UtilModelMapper() {}
+
+  public static <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
+    return source
+      .stream()
+      .map(element -> modelMapper.map(element, targetClass))
+      .collect(Collectors.toList());
+  }
+}
