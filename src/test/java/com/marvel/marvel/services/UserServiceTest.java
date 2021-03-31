cf77fbb9c57f8c0898e9eb96f13ef452f4cc48ad
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-03-31 00:45:14 -0300

    test(services): create with doc

--- src/test/java/com/marvel/marvel/services/UserServiceTest.java
+++ src/test/java/com/marvel/marvel/services/UserServiceTest.java
@@ -1,21 +1,22 @@
 package com.marvel.marvel.services;
 
+import java.util.Date;
+import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
+import com.github.javafaker.Faker;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import com.github.javafaker.Faker;
+
+import com.marvel.marvel.entities.Document;
 import com.marvel.marvel.entities.User;
-import com.marvel.marvel.repositories.UserRepository;
 
 @TestInstance(Lifecycle.PER_CLASS)
 @SpringBootTest
@@ -46,8 +47,35 @@ public class UserServiceTest {
     Date birthday = faker.date().between(from, to);
 
     User user = userService.create(name, email, password, phone, gender, birthday);
+
     assertEquals(name, user.getName());
     assertNotEquals(password, user.getPassword());
     assertNotNull(user.getId());
+  }
+
+  @Test
+  public void testCreateUserWithDocuments() throws ParseException {
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    Date from = formatter.parse("01/01/1950");
+    Date to = formatter.parse("31/12/2000");
+
+    String name = faker.name().fullName();
+    String email = faker.internet().emailAddress();
+    String password = faker.internet().password();
+    String phone = faker.phoneNumber().cellPhone();
+    String gender = faker.options().option("female", "male", "other");
+    Date birthday = faker.date().between(from, to);
+
+    Document doc1 = new Document("ssn", faker.idNumber().ssnValid());
+    Document doc2 = new Document("id", faker.idNumber().ssnValid());
+
+    List<Document> documents = List.of(doc1, doc2);
+
+    User user = userService.create(name, email, password, phone, gender, birthday, documents);
+
+    assertEquals(name, user.getName());
+    assertNotEquals(password, user.getPassword());
+    assertNotNull(user.getId());
+    assertTrue(user.getDocuments().size() > 0);
   }
 }
