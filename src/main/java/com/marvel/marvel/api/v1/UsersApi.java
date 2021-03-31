9a62c9e40f1a60d7fb49564f371ec4a8b3a5f63e
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-03-31 01:46:34 -0300

    feat(api): add get users

--- src/main/java/com/marvel/marvel/api/v1/UsersApi.java
+++ src/main/java/com/marvel/marvel/api/v1/UsersApi.java
@@ -1,18 +1,26 @@
 package com.marvel.marvel.api.v1;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.marvel.marvel.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
+import com.marvel.marvel.dto.UserDto;
+import com.marvel.marvel.entities.User;
+import com.marvel.marvel.util.UtilModelMapper;
 
 @RestController
-@RequestMapping(name = "/users")
-public class UsersApi {
-  
-  @GetMapping
-  @ResponseBody
-  public  listUsers() {
 
+public class UsersApi {
+
+  @Autowired
+  private UserService userService;
+  
+  @GetMapping("/users")
+  @ResponseBody
+  public List<UserDto> listUsers() {
+    List<User> users = userService.findAll();
+    return UtilModelMapper.mapList(users, UserDto.class);
   }
 }
