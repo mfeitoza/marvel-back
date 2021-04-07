70b728c3928c174f2051adfccf95fbf5654bfa4b
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-04-07 14:47:24 -0300

    feat(api): get user by id

--- src/main/java/com/marvel/marvel/api/v1/UsersApi.java
+++ src/main/java/com/marvel/marvel/api/v1/UsersApi.java
@@ -1,13 +1,17 @@
 package com.marvel.marvel.api.v1;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.marvel.marvel.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
+import java.util.Optional;
 import com.marvel.marvel.dto.UserDto;
 import com.marvel.marvel.dto.CreateUserDto;
 import com.marvel.marvel.entities.User;
@@ -31,5 +35,16 @@ public class UsersApi {
   public UserDto createUser(@RequestBody CreateUserDto newUser) {
     User user = userService.create(newUser);
     return UtilModelMapper.map(user, UserDto.class);
+  }
+
+  @GetMapping("/users/{id}")
+  @ResponseBody
+  public UserDto getUser(@PathVariable String id) {
+    Optional<User> user = userService.findById(id);
+    if (user.isPresent()) {
+      return UtilModelMapper.map(user.get(), UserDto.class);
+    } else {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+    }
   }
 }
