a5d94bc01d522e44c5f60a97b1787178e22e4341
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-04-07 14:27:44 -0300

    feat(api): add post users

--- src/main/java/com/marvel/marvel/api/v1/UsersApi.java
+++ src/main/java/com/marvel/marvel/api/v1/UsersApi.java
@@ -2,16 +2,18 @@ package com.marvel.marvel.api.v1;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import com.marvel.marvel.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
 import com.marvel.marvel.dto.UserDto;
+import com.marvel.marvel.dto.CreateUserDto;
 import com.marvel.marvel.entities.User;
 import com.marvel.marvel.util.UtilModelMapper;
 
 @RestController
-
 public class UsersApi {
 
   @Autowired
@@ -22,5 +24,12 @@ public class UsersApi {
   public List<UserDto> listUsers() {
     List<User> users = userService.findAll();
     return UtilModelMapper.mapList(users, UserDto.class);
+  }
+
+  @PostMapping("/users")
+  @ResponseBody
+  public UserDto createUser(@RequestBody CreateUserDto newUser) {
+    User user = userService.create(newUser);
+    return UtilModelMapper.map(user, UserDto.class);
   }
 }
